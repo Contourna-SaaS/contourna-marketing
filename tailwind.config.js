2d061a25d4a6445be1816bcd668f30d4275d003c
Tyler Olthuizen <tylerolthuizen@gmail.com>
2023-01-28 09:50:24 -0700

    updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
     extend: {
       colors: {
         'c-yellow': '#fdb913',
+        'c-yellow-2': '#fbc312',
+        'c-yellow-3': '#f7c937',
+        'c-yellow-4': '#f4c951',
+        'c-yellow-5': '#f7d06f',
+        'c-yellow-6': '#fef4df',
         'c-grey': '#494949',
         'c-off-white': '#FCFAF7',
       },
