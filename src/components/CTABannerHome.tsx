586caca4bdbe5fcc213dcca81f0cd5ae7d0e8ca3
Tyler Olthuizen <tylerolthuizen@gmail.com>
2023-01-29 13:11:13 -0700

    center align home cta

--- src/components/CTABannerHome.tsx
+++ src/components/CTABannerHome.tsx
@@ -3,7 +3,7 @@ export default function CTABannerHome() {
     <div className="container">
       <div className="mx-auto py-16 mt-8 mb-6">
         <div className="overflow-hidden rounded-lg bg-c-yellow shadow-md lg:grid lg:grid-cols-2 lg:gap-4">
-          <div className="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
+          <div className="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20 flex">
             <div className="lg:self-center">
               <h2 className="text-3xl font-bold tracking-tight text-c-grey sm:text-4xl">
                 <span className="block">
@@ -26,7 +26,7 @@ export default function CTABannerHome() {
           </div>
           <div className="aspect-w-5 aspect-h-3 -mt-6 md:aspect-w-2 md:aspect-h-1">
             <img
-              className="translate-x-6 translate-y-6 transform rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
+              className="translate-x-6 translate-y-6 transform rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20 w-1/2"
               src="images/get-a-quote-demo-blur.png"
               alt="Get a quote screenshot"
             />
